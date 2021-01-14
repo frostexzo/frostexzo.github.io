8ca8df8c7adc460779bae9bbbe2b3a99ef042a0e
exzolink <exzolink@gmail.com>
2021-01-15 02:42:24 +0300

    Grad Update

--- projects/grad/js/main.js
+++ projects/grad/js/main.js
@@ -1,12 +1,21 @@
 "use strict";
 
 const lazyEls = document.querySelectorAll(".lazy");
+const briefEl = document.querySelector(".brief-label");
 const interactiveBtns = document.querySelectorAll(".js-button");
 const modalCloseBtns = document.querySelectorAll(".modal-close");
 const modalBg = document.querySelector(".modal-background");
 const modalNav = document.querySelector(".modal-nav");
 const modalCall = document.querySelector(".modal-call");
 const modalTel = document.querySelector(".modal-tel");
+const modalPrivacy = document.querySelector(".modal-privacy");
+
+const ratingEl = document.querySelector(".last__rating");
+const ratingResult = document.querySelector(".last__rating_result");
+const ratingThumbs = document.querySelectorAll(".last__rating_thumb");
+
+const teamBtns = document.querySelectorAll(".team__selectors .tab__selector");
+const teamPersons = document.querySelectorAll(".team__item");
 let mapFlag = true;
 
 window.addEventListener("load", () => {
@@ -19,7 +28,83 @@ window.addEventListener("load", () => {
 	}, 200);
 });
 
+const initMap = () => {
+	mapFlag = false;
+	mapboxgl.accessToken =
+		"pk.eyJ1IjoiZ21ibG9nZ2VycyIsImEiOiJja2M2aGZtdjUwYjN1MnlwODk4NDNuMGxtIn0._IIb-Ofy4fn2tJGzZoWgZA";
+	const map = new mapboxgl.Map({
+		container: "map",
+		style: "mapbox://styles/mapbox/dark-v10",
+		center: [76.945997, 43.230079],
+		zoom: 16,
+	});
+
+	const marker = new mapboxgl.Marker()
+		.setLngLat([76.946597, 43.230089])
+		.addTo(map);
+};
+
 try {
+	if (teamBtns.length != 0) {
+		teamBtns.forEach((btn) => {
+			btn.addEventListener("click", () => {
+				if (btn.classList.contains("active")) return;
+
+				const btnData = btn.dataset.btn;
+				for (let i = 0; i < teamBtns.length; i++) {
+					teamBtns[i].classList.remove("active");
+				}
+
+				for (let i = 0; i < teamPersons.length; i++) {
+					teamPersons[i].classList.remove("notActive");
+				}
+
+				btn.classList.add("active");
+
+				const categoryPersons = document.querySelectorAll(
+					`.team__item:not([data-team="${btnData}"])`,
+				);
+				for (let i = 0; i < categoryPersons.length; i++) {
+					categoryPersons[i].classList.add("notActive");
+				}
+			});
+		});
+	}
+
+	if (ratingThumbs.length != 0) {
+		ratingThumbs.forEach((thumb) => {
+			thumb.addEventListener("mouseover", () => {
+				const thumbData = thumb.dataset.rating;
+
+				for (let i = 0; i < ratingThumbs.length; i++) {
+					ratingThumbs[i].classList.remove("active");
+				}
+
+				for (let i = 0; i < +thumbData; i++) {
+					ratingThumbs[i].classList.add("active");
+				}
+			});
+
+			thumb.addEventListener("click", () => {
+				const result = ["4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9"];
+				const resultIdx = Math.floor(Math.random() * result.length);
+				ratingEl.classList.add("selected");
+				ratingResult.textContent = result[resultIdx];
+				const thumbData = thumb.dataset.rating;
+
+				for (let i = 0; i < +thumbData; i++) {
+					ratingThumbs[i].classList.add("selected");
+				}
+			});
+		});
+
+		ratingEl.addEventListener("mouseleave", () => {
+			for (let i = 0; i < ratingThumbs.length; i++) {
+				ratingThumbs[i].classList.remove("active");
+			}
+		});
+	}
+
 	interactiveBtns.forEach((btn) => {
 		btn.addEventListener("click", () => {
 			const btnData = btn.dataset.btn;
@@ -44,6 +129,11 @@ try {
 					modalTel.classList.remove("active");
 					break;
 				}
+				case "privacy": {
+					modalBg.classList.add("active");
+					modalPrivacy.classList.add("active");
+					break;
+				}
 			}
 		});
 	});
@@ -55,37 +145,35 @@ try {
 	});
 	modalBg.addEventListener("click", () => {
 		const allModals = document.querySelectorAll(".modal.active");
-		modalBg.classList.remove('active');
+		modalBg.classList.remove("active");
 		for (let i = 0; i < allModals.length; i++) {
 			allModals[i].classList.remove("active");
 		}
 	});
 
-	window.addEventListener("scroll", () => {
-		if (window.pageYOffset > 2500 && mapFlag) {
-			mapFlag = false;
-			mapboxgl.accessToken =
-				"pk.eyJ1IjoiZ21ibG9nZ2VycyIsImEiOiJja2M2aGZtdjUwYjN1MnlwODk4NDNuMGxtIn0._IIb-Ofy4fn2tJGzZoWgZA";
-			const map = new mapboxgl.Map({
-				container: "map",
-				style: "mapbox://styles/mapbox/dark-v10",
-				center: [76.945997, 43.230079],
-				zoom: 16,
-			});
+	if (document.body.classList.contains("mainpage")) {
+		window.addEventListener("scroll", () => {
+			if (window.pageYOffset > 2500 && mapFlag) {
+				initMap();
+			}
 
-			const marker = new mapboxgl.Marker()
-				.setLngLat([76.946597, 43.230089])
-				.addTo(map);
+			if (window.pageYOffset > 667 && window.innerWidth <= 600) {
+				briefEl.classList.add("active");
+			} else {
+				briefEl.classList.remove("active");
+			}
+		});
+	}
 
-			window.removeEventListener("scroll");
-		}
-	});
+	if (document.querySelector(".contacts-page")) {
+		initMap();
+	}
 
 	const brandsSlider = new Swiper(".brands__container", {
 		slidesPerView: "auto",
 		freeMode: true,
 		loop: true,
-		speed: 60000,
+		speed: 90000,
 		onlyExternal: true,
 		spaceBetween: 90,
 		allowTouchMove: false,
@@ -100,4 +188,6 @@ try {
 			slidesPerView: "auto",
 		});
 	}
-} catch (err) {}
+} catch (err) {
+	console.log(err);
+}
