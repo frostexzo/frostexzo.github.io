1da6ba8fa0119a99312f9f1dfcfcb300da268780
exzolink <exzolink@gmail.com>
2021-06-06 16:39:00 +0300

    Update

--- projects/ships/app/js/app.js
+++ projects/ships/app/js/app.js
@@ -36,4 +36,27 @@ document.addEventListener("DOMContentLoaded", () => {
 		$(".modal-filters-wrapper").hide();
 		$(".filter-btn").removeClass("active");
 	});
+
+	$(".fullscreen-btn").on("click", function (e) {
+		e.preventDefault();
+		$(".header, .footer, .filter").hide();
+		$(".content").addClass("fullscreen");
+		$(".content-close").show();
+	});
+
+	$(".content-button--close").on("click", function (e) {
+		e.preventDefault();
+		$(".header, .footer, .filter").show();
+		$(".content").removeClass("fullscreen");
+		$(".content-close").hide();
+	});
+
+	$(".filter-selected").on("click", function () {
+		$(this).toggleClass('active');
+		$(this).closest(".filter-select").find(".filter-fields").slideToggle();
+	})
+
+	$(".filter-field").on("click", function () {
+		$(this).closest(".filter-select").find(".filter-selected span").text($(this).text());
+	})
 });
