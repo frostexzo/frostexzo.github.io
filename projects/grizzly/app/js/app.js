f2fc16349250dc6924d074b51ec79a3d81b93fc1
exzolink <exzolink@gmail.com>
2021-06-14 03:14:35 +0300

    Grizzly Mainpage finished

--- projects/grizzly/app/js/app.js
+++ projects/grizzly/app/js/app.js
@@ -4,58 +4,81 @@ const app = () => {
 	return {
 		isSearchActive: false,
 		isMenuOpen: false,
+		isMoreActive: false,
 		bgPart: false,
 		bgFull: false,
 
 		toggleSearch() {
 			this.isSearchActive = !this.isSearchActive;
 			this.bgPart = !this.bgPart;
-			html.classList.toggle('overflow');
+			html.classList.toggle("overflow");
 		},
 
 		toggleMenu() {
 			this.isMenuOpen = !this.isMenuOpen;
 			this.bgFull = !this.bgFull;
-			html.classList.toggle('overflow');
+			html.classList.toggle("overflow");
+		},
+
+		showMore() {
+			this.isMoreActive = true;
 		},
 
 		bgClick() {
-			this.isMenuOpen = false,
-			this.isSearchActive = false;
+			(this.isMenuOpen = false), (this.isSearchActive = false);
 			this.bgPart = false;
 			this.bgFull = false;
-		}
-	}
-}
+			html.classList.remove("overflow");
+		},
+	};
+};
 
-
-const popularSliderEl = document.querySelector('.popular__items_container');
+const popularSliderEl = document.querySelector(".popular__items_container");
 if (popularSliderEl) {
 	const popularSlider = new Swiper(".popular__items_container", {
-		slidesPerView: 5,
 		spaceBetween: 15,
 		navigation: {
 			prevEl: ".popular .swiper-button-prev",
 			nextEl: ".popular .swiper-button-next",
 		},
 		breakpoints: {
-
+			1100: {
+				slidesPerView: 5,
+			},
+			900: {
+				slidesPerView: 4,
+			},
+			650: {
+				slidesPerView: 3,
+				centeredSlides: false,
+			},
+			320: {
+				slidesPerView: "auto",
+				centeredSlides: true,
+				loop: true,
+			},
 		},
 	});
 }
 
-const feedbackSliderEl = document.querySelector('.feedback__container');
-if (popularSliderEl) {
+const feedbackSliderEl = document.querySelector(".feedback__container");
+if (feedbackSliderEl && window.innerWidth > 600) {
 	const feedbackSlider = new Swiper(".feedback__container", {
-		slidesPerView: 3,
 		spaceBetween: 20,
 		navigation: {
 			prevEl: ".feedback .swiper-button-prev",
 			nextEl: ".feedback .swiper-button-next",
 		},
 		breakpoints: {
-
+			900: {
+				slidesPerView: 3,
+			},
+			600: {
+				slidesPerView: 2,
+			},
+			320: {
+				slidesPerView: 1,
+			},
 		},
 	});
 }
-
