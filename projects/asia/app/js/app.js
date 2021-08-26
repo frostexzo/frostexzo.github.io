770b759a0153b4f7490bc571d41237ef8a559532
exzolink <exzolink@gmail.com>
2021-08-26 21:49:31 +0300

    Asia Media Update

--- projects/asia/app/js/app.js
+++ projects/asia/app/js/app.js
@@ -1,7 +1,17 @@
 const html = document.documentElement;
 
 const app = () => {
-	return {
-		
-	};
+	return {};
 };
+
+const teamSlider = document.getElementById("team");
+if (teamSlider) {
+	new Swiper(".team__container", {
+		slidesPerView: 4,
+		spaceBetween: 110,
+		navigation: {
+			nextEl: ".team__slider_pagination-next",
+			prevEl: ".team__slider_pagination-prev",
+		},
+	});
+}
