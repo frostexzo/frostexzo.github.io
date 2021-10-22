4a960a1a68948a9a58227f588f8085e875550ea6
exzolink <exzolink@gmail.com>
2021-10-22 06:03:13 +0300

    Travel Update

--- projects/travel/app/js/app.js
+++ projects/travel/app/js/app.js
@@ -1,4 +1,5 @@
 const html = document.documentElement;
+const altContainer = document.querySelectorAll(".container-alt");
 
 const app = () => {
 	return {
@@ -9,8 +10,10 @@ const app = () => {
 
 
 		selectCity(e) {
+			if (!e.classList.contains("search-tours__city_button")) return;
 			this.selectedCityId = e.dataset.id;
-			this.selectedCityName = e.innerHTML;
+			this.selectedCityName = e.innerText;
+			this.cityPopup = false;
 		}
 	};
 };
@@ -40,4 +43,69 @@ for (let i = 0; i < headerHotelsSliders.length; i++) {
 		slidesPerView: 'auto',
 		spaceBetween: 20,
 	})
+}
+
+const hotSlider = document.getElementById("hot-slider");
+if (hotSlider && window.innerWidth >= 500) {
+	new Swiper(hotSlider, {
+		slidesPerView: 'auto',
+		spaceBetween: 50,
+		slidesOffsetAfter: 15,
+		navigation: {
+			nextEl: '.hot .swiper-button-next',
+			prevEl: '.hot .swiper-button-prev',
+		  },
+		breakpoints: {
+			1100: {
+				spaceBetween: 50,
+			},
+			600: {
+				spaceBetween: 25,
+			},
+			320: {
+				spaceBetween: 15,
+			}
+		}
+	});
+}
+
+const reviewsSlider = document.getElementById("reviews-slider");
+if (reviewsSlider && window.innerWidth >= 500) {
+	new Swiper(reviewsSlider, {
+		slidesPerView: 'auto',
+		spaceBetween: 30,
+		slidesOffsetAfter: 15,
+		navigation: {
+			nextEl: '.reviews .swiper-button-next',
+			prevEl: '.reviews .swiper-button-prev',
+		  },
+		breakpoints: {
+			1100: {
+				spaceBetween: 50,
+			},
+			600: {
+				spaceBetween: 25,
+			},
+			320: {
+				spaceBetween: 15,
+			}
+		}
+	});
+}
+
+if (altContainer.length > 0) {
+	window.addEventListener("resize", () => {
+		altContainer.forEach(container => {
+			let distanceToLeft = document
+			.querySelector(".container")
+			.getBoundingClientRect();
+			container.style.marginLeft = `${distanceToLeft.left}px`;
+		})
+	});
+	altContainer.forEach(container => {
+		let distanceToLeft = document
+			.querySelector(".container")
+			.getBoundingClientRect();
+			container.style.marginLeft = `${distanceToLeft.left}px`;
+	});
 }
